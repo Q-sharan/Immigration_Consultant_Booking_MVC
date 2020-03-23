b4677abcfaef50cdf189456664306a6af0e519d3
Sharandeep <singhsharandeep75@gmail.com>
2020-03-23 19:18:31 +1300

    Add project files.

---
+++ Immigration_Consultant_Booking_MVC/Data/Migrations/20200222124638_Agency.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Immigration_Consultant_Booking_MVC.Data.Migrations
+{
+    public partial class Agency : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
